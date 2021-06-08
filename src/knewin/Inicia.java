6b5cb0acd5b4edf9cb382ca08a4bee750f4be5ec
lucascruz9494 <83309201+lucascruz9494@users.noreply.github.com>
2021-06-08 17:25:30 -0300

    Update Inicia.java

--- src/knewin/Inicia.java
+++ src/knewin/Inicia.java
@@ -17,8 +17,7 @@ public class Inicia {
 	protected static String tituloXpath = "page-title-1";
 	protected static String subXpath = "article-lead";
 	protected static String autorXpath = "author-name";
-	protected static String conteudoXpath = "/html/body/div[4]/article/div[2]/div[2]";
-	protected static String conteudoXpathDois = "/html/body/div[4]/article/div[3]/div[2]";
+	protected static String conteudoXpath = "col-md-9";
 	protected static String dataXpath = "/html/body/div[4]/article/div[1]/div/div/div[2]/div[1]/div[2]/span[2]/span/time[1]";
 	protected static Integer numero = 1;
 	public static WebDriver driver;
